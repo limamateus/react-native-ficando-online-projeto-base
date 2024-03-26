f8857f0a863b9c8163a7b3cd6801bec866678b67
Mateus Rodrigues De Lima <70700338+limamateus@users.noreply.github.com>
2024-03-25 21:35:39 -0300

    1 - Instalando dependencias e configurando um api local usando json serve

---
+++ src/paginas/Principal/index.js
@@ -0,0 +1,62 @@
+import React, { useState } from 'react';
+import { Text, View, Image, TouchableOpacity, TextInput, Alert, ScrollView } from 'react-native';
+import estilos from './estilos';
+import api from '../../servicos/api';
+export default function Principal({ navigation }) {
+    const [nomeUsuario, setNomeUsuario] = useState('');
+    const [usuario, setUsuario] = useState({});
+
+    function Buscar(){
+        api.get('/users')
+        .then(
+            resposta => {
+                console.log(resposta.data)
+            }
+        ).catch(error => {
+            console.log(error)
+        })
+    }
+    return (
+        <ScrollView>
+            <View style={estilos.container}>
+                <>
+                    <View style={estilos.fundo} />
+                    <View style={estilos.imagemArea}>
+                        <Image source={{ uri: 'https://avatars.githubusercontent.com/u/54721131?v=4' }} style={estilos.imagem} />
+                    </View>
+                    <Text style={estilos.textoNome}>Nome do usuario</Text>
+                    <Text style={estilos.textoEmail}>Email do usuario</Text>
+                    <View style={estilos.seguidoresArea}>
+                        <View style={estilos.seguidores}>
+                            <Text style={estilos.seguidoresNumero}>30</Text>
+                            <Text style={estilos.seguidoresTexto}>Seguidores</Text>
+                        </View>
+                        <View style={estilos.seguidores}>
+                            <Text style={estilos.seguidoresNumero}>40</Text>
+                            <Text style={estilos.seguidoresTexto}>Seguindo</Text>
+                        </View>
+                    </View>
+                    <TouchableOpacity onPress={() => navigation.navigate('Repositorios')}>
+                        <Text style={estilos.repositorios}>
+                            Ver os repositórios
+                        </Text>
+                    </TouchableOpacity>
+                </>
+
+                <TextInput
+                    placeholder="Busque por um usuário"
+                    autoCapitalize="none"
+                    style={estilos.entrada}
+                />
+
+                <TouchableOpacity
+                    onPress={() => Buscar()}
+                style={estilos.botao}>
+                    <Text style={estilos.textoBotao}>
+                        Buscar
+                    </Text>
+                </TouchableOpacity>
+            </View>
+        </ScrollView>
+    );
+}
